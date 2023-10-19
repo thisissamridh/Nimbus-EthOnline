8583a4fb700359467be9d75e2c4f261f35b1857c
thisissamridh <samridhsinghind@gmail.com>
2023-10-19 19:00:00 +0530

    app.js push tested final nvm16

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import React, { useEffect, useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import Approve from "./pages/Approve";
+import Buyer from "./pages/Buyer";
+import Landing from "./pages/Landing";
+import Seller from "./pages/Seller"
+import Warranty from "./pages/Warranty";
+import Web3Context from "./contexts";
+import CreateSeller from "./pages/CreateSeller";
+import CreateNFT from "./pages/CreateNFT";
+import Resell from "./pages/Resell";
+import History from "./pages/History";
+import Footer from './components/Footer';
 
 function App() {
+  window.ethereum && window.ethereum.on('accountsChanged', function (accounts) {
+    setTimeout(window.location.reload(false), 1000);
+  });
+
+  //CheckIfWallet is Connectd
+  const { checkIfWalletIsConnected } = useContext(Web3Context);
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/createseller" element={<CreateSeller />} />
+        <Route path="/seller/:add" element={<Seller />} />
+        <Route path="/buyer/:add" element={<Buyer />} />
+        <Route path="/createnft/:add" element={<CreateNFT />} />
+        <Route path="/approve/:warrantyID" element={<Approve />} />
+        <Route path="/resell/:warrantyID" element={<Resell />} />
+        <Route path="/history/:warrantyID" element={<History />} />
+        <Route path="/warranty/:warrantyID" element={<Warranty />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
